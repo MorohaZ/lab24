9c4d67649912a4268ae0b69b8bc81251bd1fc8e2
MorohaZ <141795069+MorohaZ@users.noreply.github.com>
2024-03-05 01:57:47 +0700

    com

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -8,11 +8,3 @@ int count(const int* data, size_t size) {
     return uniqueNumbers.size();
 }
 
-int main() {
-    int data[] = {1, 2, 4, 5, 4, 8, 2, 1, 2, 4, 6, 1, 4, 4, 4, 2, 1, 0, 12};
-
-    cout << "There are " << count(data, sizeof(data) / sizeof(int));
-    cout << " different numbers in data.";
-
-    return 0;
-}
